3169d4a5a64eb7c9261cc344e18cd5a4208d2de2
Asalef <asalef10@gmail.com>
2021-09-17 00:35:14 +0300

    addReactLazy

--- client/src/Router/Router.js
+++ client/src/Router/Router.js
@@ -1,13 +1,19 @@
 /* eslint-disable no-unused-vars */
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import HomePage from '../component/Pages/HomePage';
-import MovieDetails from '../component/Pages/MovieDetails';
+// import HomePage from '../component/Pages/HomePage';
+// import MovieDetails from '../component/Pages/MovieDetails';
 import SearchMovie from '../component/Fetchers/SearchMovie';
-import Register from '../component/Pages/RegisterToWeb';
-import LoginToWeb from '../component/Pages/LogInToWeb';
+// import Register from '../component/Pages/RegisterToWeb';
+// import LoginToWeb from '../component/Pages/LogInToWeb';
+const HomePage = React.lazy(()=>import('../component/Pages/HomePage'))
+const MovieDetails = React.lazy(()=>import('../component/Fetchers/SearchMovie'))
+const Register = React.lazy(()=>import('../component/Pages/RegisterToWeb'))
+const LoginToWeb = React.lazy(()=>import('../component/Pages/LogInToWeb'))
 export default function AppRouter() {
   return (
     <>
+      <React.Suspense fallback={<p>Loading</p>}>
       <Switch>
         <Route exact path="/">
           <HomePage />
@@ -20,8 +26,8 @@ export default function AppRouter() {
         <Route path="/SearchMovie/:DataSearch">
           <SearchMovie />
         </Route>
-
       </Switch>
+      </React.Suspense>
     </>
   );
 }
